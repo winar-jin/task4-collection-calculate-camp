e6256691cf24c38fe869035d56e2a562e387688b
winar-jin <winar1994@outlook.com>
2017-07-29 11:25:54 +0800

    complete collectionOperator

--- practices/collectionOperator/collect_last_element.js
+++ practices/collectionOperator/collect_last_element.js
@@ -1,7 +1,8 @@
 'use strict';
 
 function collect_last_element(collection) {
-  //在这里写入代码
+  let temp = Array.from(collection);
+  return temp.pop();
 }
 
 module.exports = collect_last_element;
--- practices/collectionOperator/get_integer_interval_2.js
+++ practices/collectionOperator/get_integer_interval_2.js
@@ -1,7 +1,26 @@
 'use strict';
 
 function get_integer_interval_2(number_a, number_b) {
-  //在这里写入代码
+  let result = [];
+  let shortStart = true;
+  if (number_a > number_b) {
+    shortStart = false;
+    [number_a,number_b] = [number_b,number_a];
+  } else if(number_a === number_b) {
+    if(number_a % 2 === 0){
+      result.push(number_a);
+      return result;
+    }
+  }
+  for(let index = number_a; index <= number_b; index++){
+    if(index % 2 === 0){
+      result.push(index);
+    }
+  }
+  if(!shortStart){
+    result.reverse();
+  }
+  return result;
 }
 
 module.exports = get_integer_interval_2;
